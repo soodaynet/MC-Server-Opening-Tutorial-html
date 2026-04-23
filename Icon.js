e6d56f69f180c0ef13872d9b43c6c548f9ea436b
chaxiao-asst <sh330521@163.com>
2026-04-23 17:32:00 +0800

    Update Icon.js

--- Icon.js
+++ Icon.js
@@ -1,6 +1,6 @@
 // 动态添加站点图标
 function addFavicon() {
-    const faviconUrl = 'https://cloudflare-imgbed.sooday.dpdns.org/file/%E5%9B%BE%E5%BA%93/%E5%9B%BE%E6%A0%87/WUjF1yC7.webp';
+    const faviconUrl = '链接';
     const link = document.createElement('link');
     link.rel = 'icon';
     link.href = faviconUrl;
